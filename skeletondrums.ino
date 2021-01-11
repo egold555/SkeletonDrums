9b6aad91569856dcf44e53f5a0ffc3ce929cf326
Eric Golde <eric@golde.org>
2021-01-10 22:27:28 -0800

    Update to the skeleton drums

--- skeletondrums.ino
+++ skeletondrums.ino
@@ -1,11 +1,16 @@
 #include <FastLED.h>
 #include <Renard.h>
+//https://learn.adafruit.com/neopixels-and-servos/the-ticoservo-library
+#include "Adafruit_TiCoServo.h"
 
 #define DRUM_1_PIN 2
 #define DRUM_2_PIN 3
 #define DRUM_3_PIN 4
 #define DRUM_4_PIN 5
 #define DRUM_5_PIN 6
+
+#define SKELETON_PIN_HEAD_1_EYES 7
+#define SKELETON_PIN_HEAD_1_SERVO 8
 
 #define GUITAR_PIN_RED 22
 #define GUITAR_PIN_BLUE 24
@@ -38,7 +43,11 @@ CRGB drum3Leds[DRUM_3_LED_COUNT];
 CRGB drum4Leds[DRUM_4_LED_COUNT];
 CRGB drum5Leds[DRUM_5_LED_COUNT]; 
 
-Renard renard(Serial, 9);
+Adafruit_TiCoServo skeleton1Servo;
+
+Renard renard(Serial, 11);
+
+int prevSkeletonServo1Pos = 90;
 
 void setup()
 {
@@ -67,41 +76,17 @@ void setup()
   pinMode(GUITAR_PIN_HAND_DIRECTION, OUTPUT);
   pinMode(GUITAR_PIN_HAND_STEP, OUTPUT);
   digitalWrite(GUITAR_PIN_HAND_DIRECTION, HAND_UP); //moving UP
+
+  pinMode(SKELETON_PIN_HEAD_1_EYES, OUTPUT);
+  skeleton1Servo.attach(SKELETON_PIN_HEAD_1_SERVO);
+  skeleton1Servo.write(90);
+
   lastTimeWeMovedMotor = millis();
 }
 void loop()
 {
   moveHand();
   if(renard.processInput()) {
-
-    // int drumR = 0;
-    // int drumG = 0;
-    // int drumB = 0;
-
-    // drumR = renard.channelValue(1);
-    // drumG = renard.channelValue(2);
-    // drumB = renard.channelValue(3);
-    // fill_solid(drum1Leds, DRUM_1_LED_COUNT, CRGB(drumR, drumG, drumB));
-
-    // drumR = renard.channelValue(4);
-    // drumG = renard.channelValue(5);
-    // drumB = renard.channelValue(6);
-    // fill_solid(drum2Leds, DRUM_2_LED_COUNT, CRGB(drumR, drumG, drumB));
-
-    // drumR = renard.channelValue(7);
-    // drumG = renard.channelValue(8);
-    // drumB = renard.channelValue(9);
-    // fill_solid(drum3Leds, DRUM_3_LED_COUNT, CRGB(drumR, drumG, drumB));
-
-    // drumR = renard.channelValue(10);
-    // drumG = renard.channelValue(11);
-    // drumB = renard.channelValue(12);
-    // fill_solid(drum4Leds, DRUM_4_LED_COUNT, CRGB(drumR, drumG, drumB));
-
-    // drumR = renard.channelValue(13);
-    // drumG = renard.channelValue(14);
-    // drumB = renard.channelValue(15);
-    // fill_solid(drum5Leds, DRUM_5_LED_COUNT, CRGB(drumR, drumG, drumB));
 
     int brightness = 0;
 
@@ -152,17 +137,23 @@ void loop()
       digitalWrite(GUITAR_PIN_WHITE, LOW);
     }
     
+    analogWrite(SKELETON_PIN_HEAD_1_EYES, renard.channelValue(10));
+
+    int rawHead = renard.channelValue(11);
+    int headPos = 90;
+    if(rawHead != 0) {
+      headPos = map(rawHead, 0, 255, 0, 180);
+    }
+    //constantly writing servo values jitter fix?
+    if(headPos != prevSkeletonServo1Pos) {
+      prevSkeletonServo1Pos = headPos;
+      skeleton1Servo.write(headPos);
+      
+    }
     
     FastLED.show();
   }
-/*
-  fill_solid(drum1Leds, DRUM_1_LED_COUNT, CRGB(255, 255, 255));
-  fill_solid(drum2Leds, DRUM_2_LED_COUNT, CRGB(255, 0, 0));
-  fill_solid(drum3Leds, DRUM_3_LED_COUNT, CRGB(0, 255, 0));
-  fill_solid(drum4Leds, DRUM_4_LED_COUNT, CRGB(0, 0, 255));
-  fill_solid(drum5Leds, DRUM_5_LED_COUNT, CRGB(255, 0, 255));
-  FastLED.show();
-*/
+
 }
 
 //called whenever lights turn on
